4dd3ff0ad86bb4efa42b537221ff9fc511973df0
DougFelipe <dougbti2022@gmail.com>
2024-11-01 14:58:18 -0300

    feat: adiciona estrutura inicial de pastas e arquivos para os 5 primeiros temas

--- script.js
+++ script.js
@@ -13,4 +13,39 @@ function init() {
   
   // Espera o carregamento do DOM
   document.addEventListener('DOMContentLoaded', init);
+
+  // script.js
+
+// Adiciona eventos de clique aos botões dos cards na página de temas
+document.addEventListener("DOMContentLoaded", function() {
+  const cards = document.querySelectorAll(".container .button");
+
+  cards.forEach((button, index) => {
+    button.addEventListener("click", () => {
+      let temaPath;
+      switch (index) {
+        case 0:
+          temaPath = "tecidos-epiteliais";
+          break;
+        case 1:
+          temaPath = "tecidos-conjuntivos";
+          break;
+        case 2:
+          temaPath = "tecidos-musculares";
+          break;
+        case 3:
+          temaPath = "tecidos-nervosos";
+          break;
+        case 4:
+          temaPath = "tecidos-adiposos";
+          break;
+        default:
+          console.error("Tema não encontrado");
+          return;
+      }
+      window.location.href = `temas/${temaPath}/modalidade.html`;
+    });
+  });
+});
+
   
